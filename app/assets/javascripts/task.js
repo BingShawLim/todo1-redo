16d29d26251e9cca02d4a4d1564592419a869291
Bing Shaw Lim <shaw.a.muvee@gmail.com>
2019-09-30 20:36:03 +0000

    lesson 20

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -4,7 +4,10 @@ $(function() {
 
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' + "data-id='" + task.id + "'" +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id + '" class="' + liClass + '">' + 
+      '<div class="view"><input class="toggle" type="checkbox"' + 
+      "data-id='" + task.id + "'" +
         checkedStatus + 
         '><label>' +
         task.title +
@@ -23,8 +26,16 @@ $(function() {
             task: {
               done: doneValue
             }
+         }).success(function(data) {
+          var liHtml = taskHtml(data);
+          var $li = $("#listItem-" + data.id);
+          $li.replaceWith(liHtml);
+          $('.toggle').change(toggleTask)
+
          });
     }
+
+
     $.get("/tasks").success( function (data) {
       var htmlString = "";
 
@@ -51,6 +62,7 @@ $(function() {
         var ulTodos = $('.todo-list');
         ulTodos.append(htmlString);
         $('.toggle').click(toggleTask);
+        $('.new-todo').val('');
       });
     });
 
